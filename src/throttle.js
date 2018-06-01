a1eccbcf9bb13209d0fe53a6a18c16b05da71508
pasierb <mpasierbski@gmail.com>
2018-06-01 15:06:16 +0200

    cleanup throttle from useless timewasters

--- src/throttle.js
+++ src/throttle.js
@@ -5,16 +5,11 @@ export default function({ timeWindow, maxRequests }) {
     const now = Date.now();
 
     // clear queue from stuff older than timeWindow
-    while (q.length && q[0] <= now - timeWindow) {
-      q.shift();
-    }
+    while (q.length && q[0] <= now - timeWindow) q.shift();
 
     const delay = q.length < maxRequests ? 0 : q[0] - (now - timeWindow);
 
-    if (!delay) {
-      q.push(now);
-      return fn();
-    }
+    if (!delay) return q.push(now) && fn();
 
     // reschedule "queueing" next possible execution time
     setTimeout(enque.bind(undefined, fn), delay+1);
